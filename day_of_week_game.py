8c3fd2b1b41be71e9520c30cc563363d8d935c34
uggerus <uggerus@protonmail.com>
2024-07-02 16:20:21 -0500

    Made calculate_day_of_week_function and correspoding tests

--- day_of_week_game.py
+++ day_of_week_game.py
@@ -1,3 +1,6 @@
 
 def  calculate_day_of_week(year, month, day):
-    return 2
+    t = 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4
+    if month < 3:
+        year-=1
+    return (year + year//4 - year//100 + year//400 + t[month - 1] + day) % 7
--- test_game.py
+++ test_game.py
@@ -2,3 +2,14 @@ from day_of_week_game import *
 
 def test_calculate_day_of_week():
     assert calculate_day_of_week(2000, 9, 12) == 2
+    assert calculate_day_of_week(1900, 2, 28) == 3
+    assert calculate_day_of_week(1891, 1, 1) == 4
+    assert calculate_day_of_week(1582, 10, 15) == 5
+    assert calculate_day_of_week(1624, 12, 15) == 0
+    assert calculate_day_of_week(1776, 7, 4) == 4
+    assert calculate_day_of_week(1984, 8, 11) == 6
+    assert calculate_day_of_week(2555, 3, 2) == 0
+    assert calculate_day_of_week(1646, 4, 2) == 1
+    assert calculate_day_of_week(2199, 5, 11) == 6
+    
+    
