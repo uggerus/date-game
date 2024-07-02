8f591e2b8de79866c5773907607e54ee2d6d1a05
uggerus <uggerus@protonmail.com>
2024-07-02 15:03:16 -0500

    Created main file for game and test file

---
+++ day_of_week_game.py
@@ -0,0 +1,3 @@
+
+def  calculate_day_of_week(year, month, day):
+    return 2
---
+++ test_game.py
@@ -0,0 +1,4 @@
+from day_of_week_game import *
+
+def test_calculate_day_of_week():
+    assert calculate_day_of_week(2000, 9, 12) == 2
